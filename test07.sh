4bdb3b2aa2d830101132cbecfeea62be7c2be22f
James Kroeger <jameskroeger123@gmail.com>
2021-04-26 07:55:05 +1000

    fixed test

--- test07.sh
+++ test07.sh
@@ -14,7 +14,7 @@ echo '/b/ p ; 35,/print/ d ; 64 q ' >> $TMP
 echo ' s0print0rmrf0 ; /print/ d ; /rmrf/ p ; /rmrf/,5 p ' >> $TMP
 echo '3d' >> $TMP
 echo '3,5  p' >> $TMP
-echo '  /^h/ p ; p ; 2 d ; 2,4 s8.*8d8g ; $q ; 2d ; 2p ; 2q ; 1,4 p ' >> $TMP
+echo '  /^h/ p ; p ; 2 d ; 2,4 s8.*8d8 ; $q ; 2d ; 2p ; 2q ; 1,4 p ' >> $TMP
 
 out=$(cat speed.pl | ./speed.pl -f $TMP)
 exp=$(cat speed.pl | 2041 speed -f $TMP)
